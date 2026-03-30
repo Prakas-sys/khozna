8d05f4f74643022fc0296e430c6c45209f4047ef
Prakash Balayar <prakashbalayar2692@xavier.edu.np>
2026-03-31 03:25:07 +0545

    fix: resolve android gradle 8 namespace issues for older legacy flutter plugins like flutter_app_badger

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,6 +17,14 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+    afterEvaluate {
+        project.extensions.findByType<com.android.build.gradle.LibraryExtension>()?.let { ext ->
+            if (ext.namespace == null) {
+                val groupStr = project.group.toString()
+                ext.namespace = if (groupStr.isNotEmpty()) groupStr else "com.example.${project.name}"
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
