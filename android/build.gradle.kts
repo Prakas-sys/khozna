71b6edd1ca3283499165c0ca016ab75166380c7f
Khozna Admin <khoznaapp@gmail.com>
2026-09-18 00:01:43 +0545

    j

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 val newBuildDir = File(rootProject.projectDir, "../build")
 rootProject.layout.buildDirectory.set(newBuildDir)
 
-rootProject.extra.set("kotlin_version", "2.2.0")
+rootProject.extra.set("kotlin_version", "2.1.0")
 
 allprojects {
     repositories {
@@ -41,9 +41,9 @@ subprojects {
         resolutionStrategy {
             force("androidx.core:core:1.13.1")
             force("androidx.core:core-ktx:1.13.1")
-            force("org.jetbrains.kotlin:kotlin-stdlib:2.2.0")
-            force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.2.0")
-            force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.2.0")
+            force("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
+            force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.1.0")
+            force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.1.0")
         }
     }
 }
